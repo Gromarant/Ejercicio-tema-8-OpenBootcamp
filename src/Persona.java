78c47379786dc8ba9e6bdc6bd659b1df34d64846
Mariangelica Rodriguez <gromarant@gmail.com>
2022-07-26 12:41:11 +0200

    Make ejercicio-tema-8 from Open Bootcamp

---
+++ src/Persona.java
@@ -0,0 +1,27 @@
+public class Persona {
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    public int getEdad() {
+        return Edad;
+    }
+    public void setEdad(int newEdad) {
+        this.edad = newEdad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String newNombre) {
+        this.nombre = newNombre;
+    }
+
+    public int getTelefono() {
+        return Telefono;
+    }
+    public void setTelefono(int newTelefono) {
+        this.Telefono = newTelefono;
+    }
+
+}
